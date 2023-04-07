acc9f516095679deff6bf5a20610f622ad3fd726
gregory567 <peti.tavaszi@gmail.com>
2023-04-07 22:51:39 +0200

    js pizza-counter added

--- main/script.js
+++ main/script.js
@@ -1,2 +1,18 @@
 
 
+const pizzaButton = document.getElementById("pizza-button");
+const pizzaCounter = document.getElementById("pizza-count");
+
+let count = 0;
+
+pizzaButton.addEventListener("click", function() {
+  count++;
+  pizzaCounter.innerText = count;
+});
+
+pizzaButton.addEventListener("click", function(){
+    pizzaButton.classList.add('grow-shrink');
+    setTimeout(function() {
+        pizzaButton.classList.remove('grow-shrink');
+    }, 50);
+});
