ee206193aec1d676cd2f95d93458f7ab30d16948
gregory567 <peti.tavaszi@gmail.com>
2023-04-18 16:59:30 +0200

    building entire level 2 html architecture with js & comments added & cleaned up the code

--- main/include js/level2.js
+++ main/include js/level2.js
@@ -1,23 +1,56 @@
-level2div.setAttribute("class","button-container");
 
+// find the parent element with class "row" and id "second_row"
+const secondRowElement = document.getElementById("second_row");
 
+// create a new div with class "col-3"
+const newCol3 = document.createElement("div");
+newCol3.classList.add("col-3"); // newCol3b.setAttribute("class","col-3");
 
+// create a new div with id "level2div"
+const level2Div = document.createElement("div");
+level2Div.id = "level2div"; // level2Div.setAttribute("id", "level2div");
+level2Div.classList.add("button-container");
 
-
-
-buyPizzaButton = document.createElement("button");
-
-buyPizzaButton.setAttribute("onclick","BuyPizza()");
+// create button to buy more pizzas
+const buyPizzaButton = document.createElement("button");
+buyPizzaButton.setAttribute("onclick", "BuyPizza()");
 buyPizzaButton.innerHTML = "Buy Frozen Pizza";
-level2div.appendChild(buyPizzaButton);
+level2Div.appendChild(buyPizzaButton);
 
-
-
-pizzaPriceContainer = document.createElement("p");
+// create p tag to display current price of pizza 
+const pizzaPriceContainer = document.createElement("p");
 pizzaPriceContainer.innerHTML = "Current Price: €" + parseFloat(pizzaPrice).toFixed(2) + "<br>";
-level2div.appendChild(pizzaPriceContainer);
+level2Div.appendChild(pizzaPriceContainer);
 
-pizzaStorageContainer = document.createElement("div");
-level2div.appendChild(pizzaStorageContainer);
+// create a new div with id "pizzastoragecontainer"
+const pizzaStorageContainer = document.createElement("div");
+pizzaStorageContainer.id = "pizzastoragecontainer"; // pizzaStorageContainer.setAttribute("id", "pizzastoragecontainer");
+pizzaStorageContainer.innerHTML="Frozen Pizzas: " + pizzaStorage;
+//pizzaStorageContainer.insertAdjacentHTML("afterbegin","Frozen Pizzas: " + pizzaStorage);
+level2Div.appendChild(pizzaStorageContainer);
 
-pizzaStorageContainer.insertAdjacentHTML("afterbegin","Frozen Pizzas: " + pizzaStorage);
+// Append the new level2div container to the second column
+newCol3.appendChild(level2Div);
+secondRowElement.appendChild(newCol3);
+
+
+// new function to purchase more pizzas: checks available funds, decreases funds by price of pizza,
+// increments number of pizzas in storage (frozen pizzas)
+function BuyPizza(){
+    // check if enough money available 
+    if (curMoney >= pizzaPrice){
+        // decrease money by current price of pizza 
+        curMoney-=pizzaPrice;
+        // increase number of frozen pizzas by 1
+        pizzaStorage++;
+        // display updated variables 
+        moneyCounter.innerText = parseFloat(curMoney).toFixed(2);
+        pizzaStorageContainer.innerHTML = "Frozen Pizzas: " + pizzaStorage + "<br>";
+    }
+}
+
+
+// if the pizza button is clicked, display the new (decreased) number of pizzas in storage 
+pizzaButton.addEventListener("click", function(){
+    pizzaStorageContainer.innerHTML="Frozen Pizzas: " + pizzaStorage;
+});
--- main/include js/script.js
+++ main/include js/script.js
@@ -7,12 +7,6 @@ const pizzaPlusCounter = document.getElementById("pizza-plus-counter");
 const dayCounter = document.getElementById("day-count");
 const Clock = document.getElementById("clock");
 
-//Level2
-const level2div = document.getElementById("level2div");
-let pizzaStorageContainer;
-let buyPizzaButton;
-let pizzaPriceContainer
-
 // intialize essential variables 
 var pizzaStorage=50;
 let pizzasWarmedUp = 0;
@@ -27,32 +21,35 @@ var plusCounter=0;
 var plusCounterDecreaseSpeed=75;
 var timeoutHandle;
 
-// Display the welcome modal on page load
+// display the welcome modal on page load
 $(document).ready(function() {
   $("#welcomeModal").modal("show");
-  pizzaButton.style.border="none";
+  pizzaButton.style.border="none"; // we need this line to fix the bug with the background of the pizza button
 });
 
+// this function prints the current level to the console and includes the script-file of level 2
 function GetLevel(){
   console.log("Current level: " + currentLevel);
-  if (currentLevel>=2)
-  {
+  if (currentLevel>=2){
+    // load the js script of level 2
     const scriptContainer = document.getElementById("script-container");
-      const level2ScriptTag = document.createElement("script");
-      level2ScriptTag.setAttribute("src", "./include js/level2.js");
-      scriptContainer.appendChild(level2ScriptTag);
-      
+    const level2ScriptTag = document.createElement("script");
+    level2ScriptTag.setAttribute("src", "./include js/level2.js");
+    scriptContainer.appendChild(level2ScriptTag); 
   }
   //If currentLevel = x, enable element on site
 }
 
+// print to the console on page load 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Loaded game!");
   GetLevel();
 });
 
-// function that implements the mechanism of the pizza counter
+// function that reduces the plus counter variable, 
+// increases the number of prepared pizzas and increments the money counter
 function ReducePlusCounter(){ 
+      // reduce the plus counter
       plusCounter--;
       if (plusCounter==0){
         pizzaPlusCounter.innerText="";
@@ -60,65 +57,58 @@ function ReducePlusCounter(){
         pizzaPlusCounter.innerText = "+" + plusCounter;
       }
       
+      // increment the number of warmed up pizzas 
       pizzasWarmedUp++;
+      // update the displayed number of prepared pizzas (visible for the user)
       pizzaCounter.innerText = pizzasWarmedUp;
+      // increment the current funds
       curMoney += pizzaValue;
+      // update the money counter variable (visible for the user)
       moneyCounter.innerText = parseFloat(curMoney).toFixed(2);
 
-
+      // mechanism that calls back this function as long as the plus counter is larger than 0
       if (plusCounter>0){
         setTimeout(function(){
           ReducePlusCounter();
         }, plusCounterDecreaseSpeed)
       }
-      
-      
+       
 }
 
-// function to start the reducepluscounter function with delay 
+// function to start the ReducePlusCounter function with delay 
 function startCounter(){
   timeoutHandle = window.setTimeout(function() {
     ReducePlusCounter();
-  },1500)
+  }, 1500)
 }
 
-function BuyPizza()
-{
-  if (curMoney >= pizzaPrice)
-  {
-    curMoney-=pizzaPrice;
-    pizzaStorage++;
-    moneyCounter.innerText = parseFloat(curMoney).toFixed(2);
-    pizzaStorageContainer.innerHTML = "Frozen Pizzas: " + pizzaStorage + "<br>";
-    
-  }
-}
-
-// function which determines what will happen if the pizza button is clicked
+// anonymous function which determines what will happen if the pizza button is clicked
 pizzaButton.addEventListener("click", function(){
     // as long as the initial pizza storage of 100 is not used up
     if (pizzaStorage > 0){
+      // decrease pizza storage by 1
       pizzaStorage--;
-      if (currentLevel>=2)
-      {
-        pizzaStorageContainer.innerHTML="Frozen Pizzas: " + pizzaStorage;
-      }
+
+      // decrease the size of the pizzabutton momentarily when its clicked 
       pizzaButton.style.transform = "scale(0.9)";
+      // increment plus counter and display it
       plusCounter++;
       pizzaPlusCounter.innerText = "+" + plusCounter;
-      clearTimeout(timeoutHandle); //Reset "+1" counter timeout on click
-     startCounter();
-    
-    setTimeout(function() {
-        pizzaButton.style.transform = "scale(1.05)";
-    }, 50);
+      clearTimeout(timeoutHandle); // reset "+1" counter timeout on click
+      // start ReducePlusCounter with timeout 
+      startCounter();
+      // increase the size of the pizza button above its normal size momentarily 
+      setTimeout(function() {
+          pizzaButton.style.transform = "scale(1.05)";
+      }, 50);
+
     // when the initial pizza storage of 100 is used up
     } else if (currentLevel==1) {
+      // trigger alert (might have to change this to a better alternative later)
       alert("You have no frozen pizzas left!");
+      // step up to level 2 and call GetLevel function
       currentLevel++;
       GetLevel();
-      // load the js script of level 2
-      
     }
     
 });
